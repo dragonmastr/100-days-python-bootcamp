349edfb501c10c6259f6030618604d70d0172776
Hardik Murdia <hardikmurdia@Hardiks-MacBook-Pro.local>
2025-06-28 23:37:53 +0530

    Day4 completed

--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -1,3 +1,6 @@
+import random
+from random import randint
+
 rock = '''
     _______
 ---'   ____)
@@ -24,3 +27,29 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+list_rps = [rock,paper,scissors]
+
+if user_choice >=0 and user_choice <=2:
+    print(list_rps[user_choice])
+else:
+    print("You typed an invalid number, you lose!")
+
+print("Computer chose:")
+computer_choice = random.randint(0,2)
+print(list_rps[computer_choice])
+if user_choice == 0 and computer_choice == 2:
+    print("You win!")
+elif computer_choice == 0 and user_choice == 2:
+    print("You lose!")
+elif computer_choice > user_choice:
+    print("You lose!")
+elif user_choice > computer_choice:
+    print("You win!")
+elif computer_choice == user_choice:
+    print("It's a draw!")
+
+
+
+
