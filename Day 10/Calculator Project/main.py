e5355a28ec6053db0ca563304019949117c83303
Hardik Murdia <hardikmurdia@ip-192-168-29-101.us-west-2.compute.internal>
2025-07-14 14:08:26 +0530

    Day 10

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,4 +1,37 @@
+import art
+print(art.logo +"\n")
 def add(n1, n2):
     return n1 + n2
 
+def multiply(n1,n2):
+    return n1*n2
 
+def divide(n1,n2):
+    return n1/n2
+
+def subtract(n1,n2):
+    return n1 - n2
+
+def calculator():
+    flag = True
+    result = 0
+    while flag :
+        n1 = int(input("What is the first number?:"))
+        print("+\n-\n*\n/\n")
+        operator = input("Pick an operation:")
+        n2 = int(input("What is the second number?:"))
+        match operator:
+            case "+":
+                result = add(n1,n2)
+            case "-":
+                result = subtract(n1, n2)
+            case "*":
+                result = multiply(n1,n2)
+            case "/":
+                result = divide(n1,n2)
+            case _:
+                print("wrong operator")
+                return
+        return result
+
+print(calculator())
